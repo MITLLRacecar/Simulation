72c4d25e0e38d7cf53dcc0bd4bbbfb64a6929b34
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-05-25 13:54:06 -0700

    Create user code

---
+++ RacecarSim/Assets/Scripts/User.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class User : MonoBehaviour
+{
+    public Racecar Racecar;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Racecar.Drive.set_speed_angle(10, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

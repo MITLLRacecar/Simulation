3c996aa9fa50062fe375305df1b270f7f13cf7d8
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-05-25 13:53:37 -0700

    Creating main and drive modules

---
+++ RacecarSim/Assets/Scripts/Drive.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+public class Drive : MonoBehaviour
+{
+    private enum WheelPosition
+    {
+        FrontLeft,
+        FrontRight,
+        BackLeft,
+        BackRight
+    }
+
+    public GameObject[] Wheels = new GameObject[4];
+    public WheelCollider[] WheelColliders = new WheelCollider[4];
+
+    private static readonly float[] unitySpeedScaleFactor = { 1f / 0.25f, 1f / 0.33f };
+    public const float unityMaxAngle = 20;
+
+    private float[] userSpeedScaleFactor = { 0.25f, 0.33f };
+    private float curSpeed = 0;
+    private float curAngle = 0;
+
+    private void Update()
+    {
+        this.WheelColliders[WheelPosition.BackLeft.GetHashCode()].motorTorque = this.curSpeed;
+        this.WheelColliders[WheelPosition.BackRight.GetHashCode()].motorTorque = this.curSpeed;
+
+        this.WheelColliders[WheelPosition.FrontLeft.GetHashCode()].steerAngle = this.curAngle;
+        this.WheelColliders[WheelPosition.FrontRight.GetHashCode()].steerAngle = this.curAngle;
+    }
+
+    public void set_speed_angle(float speed, float angle)
+    {
+        int isBackward = Convert.ToInt32(speed < 0);
+        this.curSpeed = speed * this.userSpeedScaleFactor[isBackward] * Drive.unitySpeedScaleFactor[isBackward];
+        this.curAngle = angle;
+    }
+
+    public void stop()
+    {
+        this.set_speed_angle(0, 0);
+    }
+
+    public void set_max_speed_scale_factor(float[] scale_factor)
+    {
+        this.userSpeedScaleFactor = scale_factor;
+    }
+
+}
---
+++ RacecarSim/Assets/Scripts/Racecar.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+
+public class Racecar : MonoBehaviour
+{
+    private Action userStart = null;
+    private Action userUpdate = null;
+    private Action userUpdateSlow = null;
+
+    private double updateSlowTime = 1;
+    private double updateSlowCounter = 0;
+
+    public Drive Drive { get; private set; }
+
+    private void Awake()
+    {
+        this.Drive = this.gameObject.GetComponent<Drive>();
+    }
+
+    private void Start()
+    {
+        this.GetComponent<Rigidbody>().centerOfMass = new Vector3(0, -0.2f, 0);
+        this.userStart?.Invoke();
+    }
+
+    private void Update()
+    {
+        this.userUpdate?.Invoke();
+
+        this.updateSlowCounter -= Time.deltaTime;
+        if (this.updateSlowCounter <= 0)
+        {
+            this.updateSlowCounter = this.updateSlowTime;
+            this.userUpdateSlow?.Invoke();
+        }
+    }
+
+    void set_start_update(Action start, Action update, Action update_slow = null)
+    {
+        this.userStart = start;
+        this.userUpdate = update;
+        this.userUpdateSlow = update_slow;
+    }
+
+    double get_delta_time()
+    {
+        return Time.deltaTime;
+    }
+
+    void set_update_slow_time(double time)
+    {
+        this.updateSlowTime = time;
+    }
+}

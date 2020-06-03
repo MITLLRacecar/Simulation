55655165aaf4719d5e65637eff3d97b3136f1e82
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-06-03 11:34:46 -0700

    Fix window size

--- Python/unity_display.py
+++ Python/unity_display.py
@@ -2,5 +2,6 @@ import cv2 as cv
 
 class Display:
     def show_image(self, image):
+        cv.namedWindow("display window", cv.WINDOW_NORMAL)
         cv.imshow("display window", image)
         cv.waitKey(1) # TODO is this line necessary?

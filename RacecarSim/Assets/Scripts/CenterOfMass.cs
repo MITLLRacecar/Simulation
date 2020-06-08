f47934ad57cc164e43ce28266fc34d15ec8524ef
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-06-08 10:31:41 -0700

    Script for setting rigidbody center of mass

---
+++ RacecarSim/Assets/Scripts/CenterOfMass.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Sets the center of mass of an object.
+/// </summary>
+public class CenterOfMass : MonoBehaviour
+{
+    /// <summary>
+    /// Center of mass, set in Unity editor.
+    /// </summary>
+    public Vector3 Com;
+
+    void Start()
+    {
+        this.GetComponent<Rigidbody>().centerOfMass = this.Com;
+    }
+}

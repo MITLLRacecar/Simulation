20ad9d590f7945f13fe32afee24b0424e87b663c
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-06-12 11:23:14 -0700

    Removed Python files (now in Student repo)

--- Python/unity_display.py
+++
@@ -1,7 +0,0 @@
-import cv2 as cv
-
-class Display:
-    def show_image(self, image):
-        cv.namedWindow("display window", cv.WINDOW_NORMAL)
-        cv.imshow("display window", image)
-        cv.waitKey(1) # TODO is this line necessary?

9305cb53087325fc9728d84362e772970859336f
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-06-13 01:22:32 -0700

    Created Lab 2B (cone parking)

---
+++ RacecarSim/Assets/Scripts/LevelManagement/ConeClick.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Moves the gameobject to the point clicked on screen.
+/// </summary>
+public class ConeClick : MonoBehaviour
+{
+    /// <summary>
+    /// The maximum click distance from the camera (in dm).
+    /// </summary>
+    private const int maxDistance = 200;
+    
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit raycastHit, ConeClick.maxDistance))
+            {
+                this.transform.position = raycastHit.point;
+            }
+        }
+    }
+}

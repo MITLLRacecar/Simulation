a593345464a89fe1dea31330601ed26b231a86de
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-06-14 02:00:53 -0700

    Working on main menu

---
+++ RacecarSim/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Handles the main menu.
+/// </summary>
+public class MainMenu : MonoBehaviour
+{
+    #region Constants
+    /// <summary>
+    /// The names of the simulation levels (in order).
+    /// </summary>
+    private static readonly List<string> levelNames = new List<string>()
+    {
+        "Demo",
+        "Lab 1: Driving in Shapes",
+        "Lab 2A: Color tape following",
+        "Lab 2B: Color Cone following",
+        "Lab 3B: Depth Cone following",
+        "Lab 3C: Depth Wall parking",
+        "Lab 4B: LIDAR Wall following",
+        "Phase 1 Challenge: Cone Slaloming",
+        "Lab 6B: Sensor Fusion"
+    };
+    #endregion
+
+    #region Public Interface
+    /// <summary>
+    /// Begins the level selected in the dropdown menu.
+    /// </summary>
+    public void BeginSimulation()
+    {
+        // Begin simulation
+    }
+    #endregion
+
+    /// <summary>
+    /// The dropdown menu used to select the simulation level.
+    /// </summary>
+    private Dropdown dropdown;
+
+    private void Awake()
+    {
+        this.dropdown = this.GetComponentInChildren<Dropdown>();
+    }
+
+    private void Start()
+    {
+        this.dropdown.ClearOptions();
+        this.dropdown.AddOptions(MainMenu.levelNames);
+    }
+}

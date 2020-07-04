71a24960ddd9bc0b559275101a492ae00884fcce
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-07-04 00:48:53 -0700

    Handle dynamic car ports

--- RacecarSim/Assets/Scripts/PythonInterface.cs
+++ RacecarSim/Assets/Scripts/PythonInterface.cs
@@ -22,11 +22,6 @@ public class PythonInterface
     private const int unityPortAsync = 5064;
 
     /// <summary>
-    /// The UDP port used by the Python RACECAR library.
-    /// </summary>
-    private const int pythonPort = 5066;
-
-    /// <summary>
     /// The IP address used for communication.
     /// </summary>
     private static readonly IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
@@ -49,12 +44,12 @@ public class PythonInterface
     /// <param name="racecar">The car which the python interface will control.</param>
     public PythonInterface(Racecar racecar)
     {
+        this.wasExitHandled = false;
+        this.isSyncConnected = false;
         this.racecar = racecar;
 
         // Establish and configure a UDP port
-        this.pythonEndPoint = new IPEndPoint(PythonInterface.ipAddress, PythonInterface.pythonPort);
         this.udpClient = new UdpClient(new IPEndPoint(PythonInterface.ipAddress, PythonInterface.unityPort));
-        this.udpClient.Connect(this.pythonEndPoint);
         this.udpClient.Client.ReceiveTimeout = PythonInterface.timeoutTime;
 
         // Initialize client to handle async calls
@@ -64,14 +59,37 @@ public class PythonInterface
         };
         this.asyncClientThread.Start();
     }
+
+    ~PythonInterface()
+    {
+        this.HandleExit();
+    }
+
     /// <summary>
-    /// Tells Python that the simulation has ended.
+    /// Closes all UDP clients and sends an exit command to Python if connected.
     /// </summary>
     public void HandleExit()
     {
-        this.udpClient.Send(new byte[] { (byte)Header.unity_exit.GetHashCode() }, 1);
-        this.udpClient.Close();
-        this.udpClientAsync.Close();
+        if (!this.wasExitHandled)
+        {
+            if (this.isSyncConnected)
+            {
+                try
+                {
+                    this.udpClient.Send(new byte[] { (byte)Header.unity_exit }, 1);
+                }
+                catch (Exception e)
+                {
+                    Debug.Log($"Unable to send exit command to Python. Error: {e}");
+                }
+
+                this.isSyncConnected = false;
+            }
+
+            this.udpClient.Close();
+            this.udpClientAsync.Close();
+            this.wasExitHandled = true;
+        }
     }
 
     /// <summary>
@@ -97,6 +115,7 @@ public class PythonInterface
     private enum Header
     {
         error,
+        connect,
         unity_start,
         unity_update,
         unity_exit,
@@ -126,14 +145,25 @@ public class PythonInterface
     }
 
     /// <summary>
+    /// True if exit was properly handled.
+    /// </summary>
+    private bool wasExitHandled;
+
+    #region Sync
+    /// <summary>
+    /// The racecar controlled by the user's Python script.
+    /// </summary>
+    private Racecar racecar;
+
+    /// <summary>
+    /// True if udpClient is connected to Python.
+    /// </summary>
+    private bool isSyncConnected;
+
+    /// <summary>
     /// The UDP client used to send packets to Python.
     /// </summary>
     private UdpClient udpClient;
-
-    /// <summary>
-    /// The UDP client used to handle async API calls from Python.
-    /// </summary>
-    private UdpClient udpClientAsync;
 
     /// <summary>
     /// The IP address and port used by the Python RACECAR library.
@@ -141,14 +171,15 @@ public class PythonInterface
     private IPEndPoint pythonEndPoint;
 
     /// <summary>
-    /// The racecar controlled by the user's Python script.
+    /// Connect the sync client to a Python script.
     /// </summary>
-    private Racecar racecar;
-
-    /// <summary>
-    /// A thread containing a UDP client to process asynchronous API calls from Python.
-    /// </summary>
-    private Thread asyncClientThread;
+    /// <param name="pythonPort">The port used by the Python script.</param>
+    private void ConnectSyncClient(int pythonPort)
+    {
+        this.pythonEndPoint = new IPEndPoint(PythonInterface.ipAddress, pythonPort);
+        this.udpClient.Connect(this.pythonEndPoint);
+        this.isSyncConnected = true;
+    }
 
     /// <summary>
     /// Handles a call to a Python function.
@@ -156,15 +187,20 @@ public class PythonInterface
     /// <param name="function">The Python function to call (start or update)</param>
     private void PythonCall(Header function)
     {
+        if (!this.isSyncConnected)
+        {
+            this.HandleError("Not connected to a Python script.");
+        }
+
         // Tell Python what function to call
-        this.udpClient.Send(new byte[] { (byte)function.GetHashCode() }, 1);
+        this.udpClient.Send(new byte[] { (byte)function }, 1);
 
         // Respond to API calls from Python until we receive a python_finished message
         bool pythonFinished = false;
         while (!pythonFinished)
         {
             // Receive a response from Python
-            byte[] data = this.SafeRecieve(this.udpClient);
+            byte[] data = this.SafeRecieve();
             if (data == null)
             {
                 break;
@@ -175,6 +211,10 @@ public class PythonInterface
             byte[] sendData;
             switch (header)
             {
+                case Header.error:
+                    HandleError("Error code sent from Python.");
+                    break;
+
                 case Header.python_finished:
                     pythonFinished = true;
                     break;
@@ -185,7 +225,7 @@ public class PythonInterface
                     break;
 
                 case Header.camera_get_color_image:
-                    this.SendFragmented(this.racecar.Camera.ColorImageRaw, 32, this.udpClient);
+                    this.SendFragmented(this.racecar.Camera.ColorImageRaw, 32);
                     break;
 
                 case Header.camera_get_depth_image:
@@ -285,21 +325,19 @@ public class PythonInterface
     /// </summary>
     /// <param name="bytes">The bytes to send (must be divisible by numPackets).</param>
     /// <param name="numPackets">The number of packets to split the data across.</param>
-    /// <param name="client">The client through which the packets are sent.</param>
-    private void SendFragmented(byte[] bytes, int numPackets, UdpClient client)
+    private void SendFragmented(byte[] bytes, int numPackets)
     {
         int blockSize = bytes.Length / numPackets;
         byte[] sendData = new byte[blockSize];
         for (int i = 0; i < numPackets; i++)
         {
             Buffer.BlockCopy(bytes, i * blockSize, sendData, 0, blockSize);
-            client.Send(sendData, sendData.Length);
+            this.udpClient.Send(sendData, sendData.Length);
 
-            byte[] response = this.SafeRecieve(client);
+            byte[] response = this.SafeRecieve();
             if (response == null || (Header)response[0] != Header.python_send_next)
             {
-                this.LogError("Unity and Python became out of sync while sending a block message. Returning to default drive mode.");
-                this.racecar.EnterDefaultDrive();
+                this.HandleError("Unity and Python became out of sync while sending a block message.");
                 break;
             }
         }
@@ -308,19 +346,18 @@ public class PythonInterface
     /// <summary>
     /// Receives a packet from Python and safely handles UDP exceptions (broken socket, timeout, etc.).
     /// </summary>
-    /// <param name="client">The client from which to receive a packet.</param>
     /// <returns>The data in the packet, or null if an error occurred.</returns>
-    private byte[] SafeRecieve(UdpClient client)
+    private byte[] SafeRecieve()
     {
         try
         {
-            return client.Receive(ref this.pythonEndPoint);
+            return this.udpClient.Receive(ref this.pythonEndPoint);
         }
         catch (SocketException e)
         {
             if (e.SocketErrorCode == SocketError.TimedOut)
             {
-                this.LogError("No message received from Python within the alloted time. Returning to default drive mode.");
+                this.HandleError("No message received from Python within the alloted time.");
                 Debug.LogError(">> Troubleshooting:" +
                     "\n1. Make sure that your Python program does not block or wait. For example, your program should never call time.sleep()." +
                     "\n2. Make sure that your program is not too computationally intensive. Your start and update functions should be able to run in under 10 milliseconds." +
@@ -329,22 +366,40 @@ public class PythonInterface
             }
             else
             {
-                this.LogError("An error occurred when attempting to receive data from Python. Returning to default drive mode.");
+                this.HandleError("An error occurred when attempting to receive data from Python.");
             }
-            this.racecar.EnterDefaultDrive();
         }
         return null;
     }
 
     /// <summary>
-    /// Logs an error to the HUD and Debug.LogError.
+    /// Handles when a sync error occurs by showing error text, sending an error response, and returning to default drive.
     /// </summary>
     /// <param name="errorText">The error text to show.</param>
-    private void LogError(string errorText)
+    private void HandleError(string errorText)
     {
-        Debug.LogError($">> Error: {errorText}");
-        this.racecar.Hud.SetMessage($"Error: {errorText}", Color.red, 5, 1);
+        Debug.LogError($">> Error: {errorText} Returning to default drive mode.");
+        this.racecar.Hud.SetMessage($"Error: {errorText} Returning to default drive mode.", Color.red, 5, 1);
+
+        if (this.isSyncConnected)
+        {
+            this.udpClient.Send(new byte[] { (byte)Header.error }, 1);
+        }
+
+        this.racecar.EnterDefaultDrive();
     }
+    #endregion
+
+    #region Async
+    /// <summary>
+    /// The UDP client used to handle async API calls from Python.
+    /// </summary>
+    private UdpClient udpClientAsync;
+
+    /// <summary>
+    /// A thread containing a UDP client to process asynchronous API calls from Python.
+    /// </summary>
+    private Thread asyncClientThread;
 
     /// <summary>
     /// Creates a UDP client to process asynchronous API calls from Python (for use by Jupyter).
@@ -352,28 +407,37 @@ public class PythonInterface
     private void ProcessAsyncCalls()
     {
         this.udpClientAsync = new UdpClient(new IPEndPoint(PythonInterface.ipAddress, PythonInterface.unityPortAsync));
-        this.udpClientAsync.Connect(this.pythonEndPoint);
         while (true)
         {
-            byte[] data = this.udpClientAsync.Receive(ref this.pythonEndPoint);
+            IPEndPoint receiveEndPoint = new IPEndPoint(PythonInterface.ipAddress, 0);
+            byte[] data = this.udpClientAsync.Receive(ref receiveEndPoint);
             Header header = (Header)data[0];
 
             byte[] sendData;
             switch (header)
             {
+                case Header.connect:
+                    if (!this.isSyncConnected)
+                    {
+                        this.ConnectSyncClient(receiveEndPoint.Port);
+                        sendData = new byte[1] { (byte)Header.connect };
+                        this.udpClientAsync.Send(sendData, sendData.Length, receiveEndPoint);
+                    }
+                    break;
+
                 case Header.camera_get_color_image:
-                    this.SendFragmented(this.racecar.Camera.GetColorImageRawAsync(), 32, this.udpClientAsync);
+                    this.SendFragmentedAsync(this.racecar.Camera.GetColorImageRawAsync(), 32, receiveEndPoint);
                     break;
 
                 case Header.camera_get_depth_image:
                     sendData = this.racecar.Camera.GetDepthImageRawAsync();
-                    this.udpClientAsync.Send(sendData, sendData.Length);
+                    this.udpClientAsync.Send(sendData, sendData.Length, receiveEndPoint);
                     break;
 
                 case Header.lidar_get_samples:
                     sendData = new byte[sizeof(float) * Lidar.NumSamples];
                     Buffer.BlockCopy(this.racecar.Lidar.Samples, 0, sendData, 0, sendData.Length);
-                    this.udpClientAsync.Send(sendData, sendData.Length);
+                    this.udpClientAsync.Send(sendData, sendData.Length, receiveEndPoint);
                     break;
 
                 default:
@@ -382,4 +446,28 @@ public class PythonInterface
             }
         }
     }
+
+    /// <summary>
+    /// Sends a large amount of data split across several packets via the async client.
+    /// </summary>
+    /// <param name="bytes">The bytes to send (must be divisible by numPackets).</param>
+    /// <param name="numPackets">The number of packets to split the data across.</param>
+    private void SendFragmentedAsync(byte[] bytes, int numPackets, IPEndPoint destination)
+    {
+        int blockSize = bytes.Length / numPackets;
+        byte[] sendData = new byte[blockSize];
+        for (int i = 0; i < numPackets; i++)
+        {
+            Buffer.BlockCopy(bytes, i * blockSize, sendData, 0, blockSize);
+            this.udpClientAsync.Send(sendData, sendData.Length, destination);
+
+            byte[] response = this.udpClientAsync.Receive(ref destination);
+            if (response == null || (Header)response[0] != Header.python_send_next)
+            {
+                this.udpClientAsync.Send(new byte[] { (byte)Header.error }, 1, destination);
+                break;
+            }
+        }
+    }
+    #endregion
 }

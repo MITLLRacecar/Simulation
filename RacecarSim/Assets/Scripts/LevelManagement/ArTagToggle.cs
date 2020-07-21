9228bc361eb8e6bc452e3e1aa2453e8e34a3a002
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-07-21 16:45:17 -0700

    Interactable AR tag for Lab 7 Jupyter

---
+++ RacecarSim/Assets/Scripts/LevelManagement/ArTagToggle.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class ArTagToggle : MonoBehaviour
+{
+    #region Constants
+    private static readonly Color[] colors = new Color[] { Color.white, Color.red, new Color(0, 0.5f, 1), Color.green };
+    #endregion
+
+    private Renderer tagRenderer;
+
+    private int colorIndex = 0;
+
+    private int rotation = 0;
+
+    private void Awake()
+    {
+        this.tagRenderer = this.GetComponentInChildren<Renderer>();
+    }
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            rotation = (rotation - 90) % 360;
+            this.transform.rotation = Quaternion.Euler(0, 0, rotation);
+        }
+
+        if (Input.GetMouseButtonUp(1))
+        {
+            this.colorIndex++;
+            this.tagRenderer.material.SetColor("_Color", colors[colorIndex % colors.Length]);
+        }
+    }
+}

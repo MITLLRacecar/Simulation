c6ea7f36f393fcdff898bcb98484c9cca176dd2f
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-08-29 02:00:49 -0700

    Adding docstring

--- RacecarSim/Assets/Scripts/LevelManagement/ResetBox.cs
+++ RacecarSim/Assets/Scripts/LevelManagement/ResetBox.cs
@@ -1,5 +1,8 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// An object which resets a racecar back to the previous checkpoint on collision.
+/// </summary>
 public class ResetBox : MonoBehaviour
 {
     private void OnTriggerEnter(Collider other)

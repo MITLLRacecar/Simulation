f0b983087cbdc51adadc6b7aed3ce7916790eb6c
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-08-30 15:48:39 -0700

    Refactored variable object to work with new variable manager

--- RacecarSim/Assets/Scripts/LevelManagement/VariableObject.cs
+++ RacecarSim/Assets/Scripts/LevelManagement/VariableObject.cs
@@ -2,58 +2,83 @@
 
 public class VariableObject : MonoBehaviour
 {
+    /// <summary>
+    /// The variable color set of the race which determines this object's color.
+    /// </summary>
     [SerializeField]
-    private VariableManager.VariableColor color;
+    private int colorSetIndex = -1;
 
+    /// <summary>
+    /// The index of this object within its color set.
+    /// </summary>
     [SerializeField]
-    private VariableManager.VariableTurn turnDirection;
+    private int colorIndex = -1;
 
+    /// <summary>
+    /// The index of the variable turn in the race which determines this object's behavior.
+    /// </summary>
     [SerializeField]
-    private int turnIndex;
+    private int turnIndex = -1;
 
+    /// <summary>
+    /// The turn direction which this object blocks.
+    /// </summary>
+    [SerializeField]
+    private VariableManager.RaceTurn turnDirection = VariableManager.RaceTurn.None;
+
+    /// <summary>
+    /// Set the color of this object to the variable color chosen for the specified set and index.
+    /// </summary>
     [SerializeField]
     private bool setColor;
 
+    /// <summary>
+    /// Set the color of this object's children to the variable color chosen for the specified set and index.
+    /// </summary>
     [SerializeField]
     private bool setColorInChild;
 
+    /// <summary>
+    /// Rotate the object 90 degrees to the left or right depending on the variable turn chosen for the specified turn index.
+    /// </summary>
     [SerializeField]
     private bool setRotation;
 
+    /// <summary>
+    /// Remove the object if the variable turn at the specified index turns in the direction of this object.
+    /// </summary>
     [SerializeField]
     private bool remove;
 
     void Start()
     {
-        if (this.color != VariableManager.VariableColor.None)
+        if (this.setColor)
         {
-            Color rgbColor = VariableManager.GetColor(this.color);
-            if (this.setColor)
-            {
-                this.GetComponent<Renderer>().material.color = rgbColor;
-            }
+            this.GetComponent<Renderer>().material.color = VariableManager.GetVariableColor(this.colorSetIndex, this.colorIndex);
+        }
 
-            if (this.setColorInChild)
-            {
-                this.GetComponentInChildren<Renderer>().material.color = rgbColor;
-            }
+        if (this.setColorInChild)
+        {
+            this.GetComponentInChildren<Renderer>().material.color = VariableManager.GetVariableColor(this.colorSetIndex, this.colorIndex);
         }
 
         if (this.setRotation)
         {
-            if (VariableManager.TurnChoices[this.turnIndex] == VariableManager.VariableTurn.Left)
+            VariableManager.RaceTurn turnDirection = VariableManager.GetVariableTurn(this.turnIndex);
+            switch (turnDirection)
             {
-                this.transform.Rotate(0, 0, 90);
-            }
-            else
-            {
-                this.transform.Rotate(0, 0, -90);
+                case VariableManager.RaceTurn.Left:
+                    this.transform.Rotate(0, 0, 90);
+                    break;
+                case VariableManager.RaceTurn.Right:
+                    this.transform.Rotate(0, 0, -90);
+                    break;
             }
         }
 
         if (this.remove)
         {
-            if (VariableManager.TurnChoices[this.turnIndex] == this.turnDirection)
+            if (VariableManager.GetVariableTurn(this.turnIndex) == this.turnDirection)
             {
                 Destroy(this.gameObject);
             }

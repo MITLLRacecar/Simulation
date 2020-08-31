efe39b137d4cd1caa8d4783bfe1d304d72023220
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-08-30 22:12:23 -0700

    Combine checkpoint and finish line into KeyPoint class

---
+++ RacecarSim/Assets/Scripts/LevelManagement/KeyPoint.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class KeyPoint : MonoBehaviour
+{
+    #region Set in Unity Editor
+    /// <summary>
+    /// Whether this key point is the finish line (true) or a checkpoint (false)
+    /// </summary>
+    [SerializeField]
+    bool isFinishLine = true;
+
+    /// <summary>
+    /// The index of the checkpoint. Not used for finish line.
+    /// </summary>
+    [SerializeField]
+    int index = 0;
+    #endregion
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Racecar car = other.attachedRigidbody.GetComponent<Racecar>();
+        if (car != null)
+        {
+            if (this.isFinishLine)
+            {
+                LevelManager.HandleFinish(car.Index);
+            }
+            else
+            {
+                LevelManager.HandleCheckpoint(this.index, car.Index);
+            }
+        }
+    }
+}

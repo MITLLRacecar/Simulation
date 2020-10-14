22d5d998a7d995d1e413540ec0fd2e8e877d2a4f
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2020-10-13 20:50:57 -0700

    Improve AR tag jupyter level

--- RacecarSim/Assets/Scripts/LevelManagement/ArTagToggle.cs
+++ RacecarSim/Assets/Scripts/LevelManagement/ArTagToggle.cs
@@ -1,74 +1,115 @@
 ﻿using UnityEngine;
 
+/// <summary>
+/// Controls an AR tag which can be modified by the user.
+/// </summary>
 public class ArTagToggle : MonoBehaviour
 {
-    #region Constants
-    private static readonly Color[] colors = new Color[] { Color.white, Color.red, new Color(0, 0.5f, 1), Color.green };
-
-    private const float scrollScale = 10;
-    #endregion
-
     #region Set in Unity Editor
+    /// <summary>
+    /// The AR tags which can be displayed on this object.
+    /// </summary>
     [SerializeField]
     private Material[] tags;
 
+    /// <summary>
+    /// The index of the AR tag to initially render.
+    /// </summary>
     [SerializeField]
     private int tagIndex = 0;
     #endregion
 
-    private Renderer outline;
+    #region Constants
+    /// <summary>
+    /// The possible colors of the AR tag.
+    /// </summary>
+    private static readonly Color[] colors = new Color[] { Color.white, Color.red, new Color(0, 0.5f, 1), Color.green };
 
-    private Renderer arTag;
+    /// <summary>
+    /// The scale applied to the mouse scroll speed when rotating the AR tag.
+    /// </summary>
+    private const float scrollScale = 10;
 
+    /// <summary>
+    /// The color of the padding of a selected AR tag.
+    /// </summary>
+    private static readonly Color selectedColor = new Color(0.75f, 0.75f, 0.75f);
+    #endregion
+
+    private enum Renderers
+    {
+        Outline,
+        Tag,
+        Padding
+    }
+
+    /// <summary>
+    /// The renders of the objects in the AR tag.
+    /// </summary>
+    private Renderer[] renderers;
+
+    /// <summary>
+    /// The index of the current color shown.
+    /// </summary>
     private int colorIndex = 0;
 
+    /// <summary>
+    /// True if the AR tag is currently selected by the user.
+    /// </summary>
     private bool selected = false;
 
     private void Awake()
     {
-        Renderer[] renderers = this.GetComponentsInChildren<Renderer>();
-        this.outline = renderers[0];
-        this.arTag = renderers[1];
+        this.renderers = this.GetComponentsInChildren<Renderer>();
     }
 
     private void Start()
     {
-        this.arTag.material = tags[tagIndex];
+        this.renderers[(int)Renderers.Tag].material = tags[tagIndex];
+        this.renderers[(int)Renderers.Padding].material.SetColor("_Color", Color.white);
     }
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            RaycastHit hit;
-            this.selected = Physics.Raycast(ray, out hit) && hit.collider.gameObject == this.gameObject;
-        }
+        bool leftMouse = Input.GetMouseButtonDown(0);
+        bool rightMouse = Input.GetMouseButtonDown(1);
+        bool tagClicked = (leftMouse || rightMouse) &&
+            Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out RaycastHit hit) &&
+            hit.collider.gameObject == this.gameObject;
 
         if (this.selected)
         {
-            if (Input.GetMouseButtonDown(1))
+            if (tagClicked)
             {
-                this.colorIndex++;
-                this.outline.material.SetColor("_Color", colors[colorIndex % colors.Length]);
+                if (leftMouse)
+                {
+                    // Toggle the displayed tag when left-clicked
+                    this.tagIndex += tags.Length + 1;
+                    this.tagIndex %= tags.Length;
+                    this.renderers[(int)Renderers.Tag].material = tags[tagIndex];
+                }
+                if (rightMouse)
+                {
+                    // Toggle the color when right-clicked
+                    this.colorIndex++;
+                    this.renderers[(int)Renderers.Outline].material.SetColor("_Color", colors[colorIndex % colors.Length]);
+                }
+            }
+            else if (leftMouse || rightMouse)
+            {
+                // Deselect the tag if we click off it
+                this.selected = false;
+                this.renderers[(int)Renderers.Padding].material.SetColor("_Color", Color.white);
             }
 
+            // Rotate the tag based on scroll amount
             this.transform.Rotate(0, 0, ArTagToggle.scrollScale * Input.mouseScrollDelta[1]);
-
-            if (Input.GetKeyDown(KeyCode.LeftControl))
-            {
-                this.tagIndex += tags.Length - 1;
-                this.tagIndex %= tags.Length;
-                this.arTag.material = tags[tagIndex];
-            }
-
-            if (Input.GetKeyDown(KeyCode.RightControl))
-            {
-                this.tagIndex++;
-                this.tagIndex %= tags.Length;
-                this.arTag.material = tags[tagIndex];
-            }
+        }
+        else if (tagClicked)
+        {
+            // Select the tag when left-clicked
+            this.selected = true;
+            this.renderers[(int)Renderers.Padding].material.SetColor("_Color", ArTagToggle.selectedColor);
         }
     }
 }

38f88093f9cc89b1b3e6357e9180c0a68e5d7639
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2021-01-01 04:02:29 -0800

    Clarify documentation

--- RacecarSim/Assets/Scripts/Static/Constants.cs
+++ RacecarSim/Assets/Scripts/Static/Constants.cs
@@ -1,7 +1,7 @@
 ﻿public static class Constants
 {
     /// <summary>
-    /// The maximum speed which the car can travel and still be considered "stopped".
+    /// The maximum speed which the car can travel in m/s and still be considered "stopped".
     /// </summary>
     public const float MaxStopSeed = 0.02f;
 }

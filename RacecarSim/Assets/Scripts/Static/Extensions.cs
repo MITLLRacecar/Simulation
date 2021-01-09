5dc9d7828fccd3652be067116f128a8d722ca814
MatthewCalligaro <MatthewCalligaro@hotmail.com>
2021-01-08 22:30:35 -0800

    Update GetComponentsInChildrenOrdered to search recursively in children

--- RacecarSim/Assets/Scripts/Static/Extensions.cs
+++ RacecarSim/Assets/Scripts/Static/Extensions.cs
@@ -7,22 +7,39 @@ using UnityEngine;
 public static class Extensions
 {
     /// <summary>
-    /// Similar to GetComponentsInChildren, except objects are returned in transform sort order and we only search through first-level children.
+    /// Similar to GetComponentsInChildren, except objects are returned in transform sort order.
     /// </summary>
     /// <typeparam name="T">The type to search for in our children.</typeparam>
     /// <param name="monoBehaviour">this</param>
-    /// <returns>An array of all T objects in the first-level children of this gameobject in transfrom sort order.</returns>
+    /// <returns>An array of all T components in all children (and grandchildren, etc.) of this gameobject in transfrom sort order.</returns>
     public static T[] GetComponentsInChildrenOrdered<T>(this MonoBehaviour monoBehaviour)
     {
+        return GetComponentsInChildrenOrdered<T>(monoBehaviour.transform).ToArray();
+    }
+
+    /// <summary>
+    /// Helper function for the extension GetComponentsInChildrenOrdered method.
+    /// </summary>
+    /// <typeparam name="T">The type to search for in the transform's children.</typeparam>
+    /// <param name="transform">The parent transform in which to search.</param>
+    /// <returns>A list of all T objects in the all children (and grandchildren, etc.) of this transform in transfrom sort order.</returns>
+    private static List<T> GetComponentsInChildrenOrdered<T>(Transform transform)
+    {
         List<T> components = new List<T>();
-        foreach (Transform child in monoBehaviour.transform)
+        foreach (Transform child in transform)
         {
             T component = child.GetComponent<T>();
             if (component != null)
             {
                 components.Add(component);
             }
+
+            // Recursively search the child's children
+            if (child.transform.childCount > 0)
+            {
+                components.AddRange(GetComponentsInChildrenOrdered<T>(child.transform));
+            }
         }
-        return components.ToArray();
+        return components;
     }
 }

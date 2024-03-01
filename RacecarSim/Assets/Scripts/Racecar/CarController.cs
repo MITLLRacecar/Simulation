5bb6dd15ba59d32ed10160722b55ac8b4036b8cb
Oxters <oxters168@gmail.com>
2024-02-29 21:18:05 -0500

    Switched out the default wheel colliders with Wheel Controller 3D components in the race car prefab and made some modifications to the drive script to accomodate

---
+++ RacecarSim/Assets/Scripts/Racecar/CarController.cs
@@ -0,0 +1,245 @@
+using NWH.Common.Vehicles;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+using NWH.NUI;
+#endif
+
+namespace NWH.WheelController3D
+{
+    /// <summary>
+    /// Wrapper for the wheel, containing wheel-specific settings
+    /// and the reference to WheelUAPI - an API that allows
+    /// for use of WheelController or WheelColliderUAPI with the CarController
+    /// (or VehicleController in NWH Vehicle Physics).
+    /// </summary>
+    [Serializable]
+    public class _Wheel
+    {
+        public bool            power;
+        public bool            steer;
+        public bool handbrake;
+        public WheelUAPI wheelUAPI;
+    }
+
+    /// <summary>
+    /// Simple vehicle controller intended as a demo script to help showcase WC3D.
+    /// If you need a complete vehicle physics package that uses WC3D check out NWH Vehicle Physics.
+    /// Owners of WC3D get 30% off:
+    /// https://assetstore.unity.com/packages/tools/physics/nwh-vehicle-physics-107332
+    /// Since it inherits from the Vehicle class it can be used with the VehicleChanger
+    /// and any of the other vehicle NWH assets (aircraft, boats) where the player can exit
+    /// one type of the vehicle and enter any other type of vehicle, since all of these
+    /// inherit from the same Vehicle class.
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody))]
+    public class CarController : Vehicle
+    {
+        /// <summary>
+        /// Maximum brake torque to be applied to the wheel.
+        /// Also used for handbrake.
+        /// </summary>
+        public float maxBrakeTorque = 3000f;
+        
+        /// <summary>
+        /// Maximum motor torque to be applied to the wheel.
+        /// Since in this simple version of car controller powertrain is not
+        /// simulated, too high motor torque can cause wheel spin up to
+        /// very high angular velocities since there is no artificial cap.
+        /// </summary>
+        public float maxMotorTorque = 1000f;
+        
+        /// <summary>
+        /// Maximum steering angle in degrees of the wheels that have steer set to true.
+        /// This angle is used at ~0 velocity and interpolated towards the minSteerAngle
+        /// as the speed increases.
+        /// </summary>
+        public float maxSteeringAngle = 35;
+        
+        /// <summary>
+        /// Minimum steering angle. Steering angle is interpolated between max and min angles
+        /// as the speed increases, with the minSteeringAngle achieved at about ~20m/s. This
+        /// prevents jerky vehicle control and spinning out at higher speeds.
+        /// Simplified implementation.
+        /// </summary>
+        public float minSteeringAngle = 20;
+        
+        /// <summary>
+        /// List of wheel containers for this vehicle.
+        /// </summary>
+        public List<_Wheel> wheels;
+
+        public float driveAxis;
+        public float steerAxis;
+
+        protected float smoothXAxis;
+        protected float xAxis;
+        protected float xAxisVelocity;
+        protected float yAxis;
+
+        private Rigidbody _rigidbody;
+        
+        
+        public override void Awake()
+        {
+            base.Awake();
+
+            // Init cached values
+            _rigidbody = GetComponent<Rigidbody>();
+            if (_rigidbody == null) gameObject.AddComponent<Rigidbody>();
+        }
+
+        
+        /// <summary>
+        /// Resets the wheel torque and steering values since
+        /// FixedUpdate() is not run when behaviour is disabled.
+        /// </summary>
+        public override void OnDisable()
+        {
+            base.OnDisable();
+
+            for (int i = 0; i < wheels.Count; i++)
+            {
+                WheelUAPI wc = wheels[i].wheelUAPI;
+                wc.BrakeTorque = maxBrakeTorque;
+                wc.MotorTorque = 0f;
+                wc.SteerAngle = 0f;
+            }
+        }
+
+        
+        /// <summary>
+        /// Since v11.13f not run when vehicle is disabled.
+        /// Previously FixedUpdate() was called on vehicles that were asleep (now
+        /// replaced with Behaviour.enabled).
+        /// </summary>
+        public override void FixedUpdate()
+        {
+            base.FixedUpdate();
+            
+            xAxis = steerAxis;
+            yAxis = driveAxis;
+            bool spaceHeld = Input.GetKey(KeyCode.Space);
+            smoothXAxis = Mathf.SmoothDamp(smoothXAxis, xAxis, ref xAxisVelocity, 0.12f);
+
+            for (int i = 0; i < wheels.Count; i++)
+            {
+                _Wheel w = wheels[i];
+                WheelUAPI wc = w.wheelUAPI;
+                wc.BrakeTorque = 0f;
+                wc.MotorTorque = 0f;
+
+                if ( w.handbrake && spaceHeld)
+                {
+                    wc.BrakeTorque = maxBrakeTorque;
+                }
+
+                if (SpeedSigned < -0.4f && yAxis > 0.1f || SpeedSigned > 0.4f && yAxis < -0.1f)
+                {
+                    wc.BrakeTorque = maxBrakeTorque * Mathf.Abs(yAxis);
+                }
+
+                if (w.power && 
+                    SpeedSigned >= -0.5f && yAxis > 0.1f || SpeedSigned <= 0.5f && yAxis < -0.1f)
+                {
+                    wc.MotorTorque = maxMotorTorque * yAxis;
+                }
+
+
+                if (w.steer)
+                {
+                    wc.SteerAngle = Mathf.Lerp(maxSteeringAngle, minSteeringAngle, Speed * 0.04f) * smoothXAxis;
+                }
+            }
+        }
+        
+        
+        /// <summary>
+        /// Set defaults.
+        /// </summary>
+        private void Reset()
+        {
+            maxBrakeTorque = 3000f;
+            maxMotorTorque = 1000f;
+            maxSteeringAngle = 35f;
+            minSteeringAngle = 20f;
+
+            wheels = new List<_Wheel>();
+
+            WheelUAPI[] wheelUAPIs = GetComponentsInChildren<WheelUAPI>();
+            for (int i = 0; i < wheelUAPIs.Length; i++)
+            {
+                WheelUAPI wheelUAPI = wheelUAPIs[i];
+                wheels.Add(new _Wheel()
+                           {
+                               wheelUAPI = wheelUAPI,
+                               steer = i < 2,
+                               power = true,
+                               handbrake = i > 1
+                           });
+            }
+        }
+    }
+
+#if UNITY_EDITOR
+    /// <summary>
+    ///     Editor for WheelController.
+    /// </summary>
+    [CustomEditor(typeof(CarController))]
+    [CanEditMultipleObjects]
+    public class CarControllerEditor : NUIEditor
+    {
+        public override bool OnInspectorNUI()
+        {
+            if (!base.OnInspectorNUI()) return false;
+
+            drawer.Info(
+                "This is a minimalistic car controller intended for demo purposes. The vehicle behavior will not be representative " +
+                "of a real vehicle since powertrain is not simulated, there are no differentials, etc. For a complete vehicle solution using WheelController3D " +
+                "check out NWH Vehicle Physics 2.");
+
+            drawer.BeginSubsection("Torque");
+            drawer.Field("maxMotorTorque");
+            drawer.Field("maxBrakeTorque");
+            drawer.EndSubsection();
+
+            drawer.BeginSubsection("Steering");
+            drawer.Field("maxSteeringAngle");
+            drawer.Field("minSteeringAngle");
+            drawer.EndSubsection();
+
+            drawer.BeginSubsection("Wheels");
+            drawer.ReorderableList("wheels");
+            drawer.EndSubsection();
+
+            drawer.EndEditor(this);
+            return true;
+        }
+    }
+
+
+    /// <summary>
+    ///     Editor for WheelController.
+    /// </summary>
+    [CustomPropertyDrawer(typeof(_Wheel))]
+    [CanEditMultipleObjects]
+    public class _WheelPropertyDrawer : NUIPropertyDrawer
+    {
+        public override bool OnNUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (!base.OnNUI(position, property, label)) return false;
+
+            drawer.Field("wheelUAPI");
+            drawer.Field("power");
+            drawer.Field("steer");
+            drawer.Field("handbrake");
+
+            drawer.EndProperty();
+            return true;
+        }
+    }
+#endif
+}
--- RacecarSim/Assets/Scripts/Racecar/Drive.cs
+++ RacecarSim/Assets/Scripts/Racecar/Drive.cs
@@ -1,54 +1,19 @@
 ﻿using System;
 using UnityEngine;
+using NWH.WheelController3D;
 
 /// <summary>
 /// Controls car physics and movement.
 /// </summary>
 public class Drive : RacecarModule
 {
-    #region Set in Unity Editor
-    /// <summary>
-    /// The models which visualize the car's wheels.
-    /// </summary>
-    [SerializeField]
-    private GameObject[] Wheels = new GameObject[4];
-
-    /// <summary>
-    /// The invisible colliders responsible for the physics of the car's wheels.
-    /// </summary>
-    public WheelCollider[] WheelColliders = new WheelCollider[4];
-    #endregion
+    private CarController carController;
 
     #region Constants
     /// <summary>
     /// The default value of MaxSpeed.
     /// </summary>
-    public const float DefaultMaxSpeed = 0.25f;
-
-    /// <summary>
-    /// The scale factor applied to the Speed input provided by the user.
-    /// </summary>
-    private const float torqueScale = 10000.0f;
-
-    /// <summary>
-    /// The natural braking torque applied to all wheels when no input torque is given.
-    /// </summary>
-    private const float brakeTorqueScale = 1.0f;
-
-    /// <summary>
-    /// The maximum angle that the front wheels can turn.
-    /// </summary>
-    private const float maxDriveAngle = 20;
-
-    /// <summary>
-    /// The number of substeps used in wheel physics calculations.
-    /// </summary>
-    private const int vehicalSubsteps = 20;
-
-    /// <summary>
-    /// The distance the wheel model is offset from the wheel collider along the axle axis.
-    /// </summary>
-    private const float wheelOffset = 0.2f;
+    public const float DefaultMaxSpeed = 1f;
     #endregion
 
     #region Public Interface
@@ -77,63 +42,17 @@ public class Drive : RacecarModule
     }
     #endregion
 
-    /// <summary>
-    /// The rigidbody of the car.
-    /// </summary>
-    private Rigidbody rBody;
-
-    /// <summary>
-    /// The four wheel positions in the order they appear in the car prefab.
-    /// </summary>
-    private enum WheelPosition
-    {
-        FrontLeft,
-        FrontRight,
-        BackLeft,
-        BackRight
-    }
-
     protected override void Awake()
     {
-        this.rBody = this.GetComponent<Rigidbody>();
+        this.carController = this.GetComponent<CarController>();
 
         base.Awake();
     }
 
-    private void Start()
-    {
-        foreach (WheelCollider wheel in this.WheelColliders)
-        {
-            wheel.ConfigureVehicleSubsteps(1, Drive.vehicalSubsteps, Drive.vehicalSubsteps);
-        }
-    }
-
     private void FixedUpdate()
     {
-        // Apply resting brake torque if Speed input is 0
-        float brakeTorque = this.Speed == 0 ? Mathf.Pow(this.rBody.velocity.magnitude, 2) * Drive.brakeTorqueScale : 0;
-        foreach (WheelCollider wheel in this.WheelColliders)
-        {
-            wheel.brakeTorque = brakeTorque;
-        }
-
-        // Set torque and angle of wheel colliders
-        float torque = this.Speed * this.MaxSpeed * Drive.torqueScale;
-        this.WheelColliders[(int)WheelPosition.BackLeft].motorTorque = torque;
-        this.WheelColliders[(int)WheelPosition.BackRight].motorTorque = torque;
-
-        float driveAngle = this.Angle * Drive.maxDriveAngle;
-        this.WheelColliders[(int)WheelPosition.FrontLeft].steerAngle = driveAngle;
-        this.WheelColliders[(int)WheelPosition.FrontRight].steerAngle = driveAngle;
-
-        // Update position and rotation of wheel models to match wheel colliders
-        foreach(WheelPosition wheelPosition in Enum.GetValues(typeof(WheelPosition)))
-        {
-            this.WheelColliders[(int)wheelPosition].GetWorldPose(out Vector3 position, out Quaternion rotation);
-            this.Wheels[(int)wheelPosition].transform.rotation = rotation;
-            this.Wheels[(int)wheelPosition].transform.position = (int)wheelPosition % 2 == 0 
-                ? position + this.Wheels[(int)wheelPosition].transform.right * Drive.wheelOffset
-                : position - this.Wheels[(int)wheelPosition].transform.right * Drive.wheelOffset;
-        }
+        // For NWH WheelController
+        this.carController.driveAxis = this.Speed * this.MaxSpeed;
+        this.carController.steerAxis = this.Angle;
     }
 }
